1ffe9dcea061a63377180875007026ffa4c80ec3
MasterAcnolo <68693319+MasterAcnolo@users.noreply.github.com>
2025-06-29 13:52:46 +0200

    Push ReadME + License

--- server/server.js
+++ server/server.js
@@ -1,3 +1,21 @@
+/*
+  This file is part of Freedom Loader.
+
+  Copyright (C) 2025 MasterAcnolo
+
+  Freedom Loader is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License.
+
+  Freedom Loader is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  See the GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 const express = require("express");
 const { exec } = require("child_process");
 const fs = require("fs");

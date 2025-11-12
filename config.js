dff5b2b55912481921f42b31c191bf2b59f93635
MasterAcnolo <68693319+MasterAcnolo@users.noreply.github.com>
2025-11-12 14:48:05 +0100

    Documentation Bouton + Package Lock Update

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ module.exports = {
     version: "1.3.0",
     applicationPort: "8787",
     debugMode: true,
-    localMode: false,
+    localMode: true,
     DiscordRPCID: "1410934537051181146",
 }

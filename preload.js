f0e91ef0a9648f767d45ebde493115bc34b33ae4
MasterAcnolo <68693319+MasterAcnolo@users.noreply.github.com>
2025-11-15 23:52:43 +0100

    Loading Bar taskbar

--- preload.js
+++ preload.js
@@ -3,4 +3,5 @@ const { contextBridge, ipcRenderer } = require("electron");
 contextBridge.exposeInMainWorld("electronAPI", {
   getDefaultDownloadPath: () => ipcRenderer.invoke("get-default-download-path"),
   selectDownloadFolder: () => ipcRenderer.invoke("select-download-folder"),
+  setProgress: (percent) => ipcRenderer.send("set-progress", percent)
 });

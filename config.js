6c9d296fbd0b0f1fe9ed6e4dcc8df85c23372831
MasterAcnolo <68693319+MasterAcnolo@users.noreply.github.com>
2025-12-30 11:38:18 +0100

    Add: Some Variables in config.js. For Modules

--- config.js
+++ config.js
@@ -7,4 +7,17 @@ module.exports = {
     debugMode: true,
     localMode: !app.isPackaged,
     DiscordRPCID: "1410934537051181146",
+
+    // Variables Used to toggle main features
+    autoUpdate: true,
+    discordRPC: true,
+    customTopBar: true, // (Will be active on next launch)
+    autoDownloadPlaylist: true,
+    logSystem: true, // Disable = Dangerous 
+    autoCheckInfo: true, // To Improve speed ? (NO)
+    outputTitleCheck: true, // For Non latin characters (Russian)
+    addThumbail: true, // The Pictures in the files (audio files)
+    addMetadata: true, // Looks Explicit
+    downloadSystem: true, // Why would you disable this ? I don't know but why not
+    notifySystem: true // Notification when download 
 }

68a2836fe0111a4c908837506f529ecc21109f3c
MasterAcnolo <68693319+MasterAcnolo@users.noreply.github.com>
2026-02-17 23:26:01 +0100

    Fix: Correct windowMs value in rate limit configuration to 15 minutes

--- server/helpers/rateLimit.js
+++ server/helpers/rateLimit.js
@@ -3,7 +3,7 @@ const RateLimit = require("express-rate-limit");
 
 /* Rate Limite */
 const rateLimite = RateLimit({
-  windowMs: 15 * 60, // 15 ms
+  windowMs: 15 * 60 * 1000, // 15 minutes
   max: 5, // limit each IP to 100 requests per windowMs on the root path
   message: "Too many requests, please try again later.",
   statusCode: 429, // HTTP status code for "Too Many Requests"

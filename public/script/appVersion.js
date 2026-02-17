f8e346b3cb50e0ad7840fdbf0afd027f77c54129
MasterAcnolo <68693319+MasterAcnolo@users.noreply.github.com>
2026-02-17 21:12:01 +0100

    Fix: Small coms in AppVersion.js

--- public/script/appVersion.js
+++ public/script/appVersion.js
@@ -1,5 +1,7 @@
 async function versionLabel(){
     const appVersion = await window.electronAPI.getVersion();
+
+    // Write in front the app version for debugging (bottom right)
     document.getElementById("version-badge").textContent = `v${appVersion}`;
 };
 
